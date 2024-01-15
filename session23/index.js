bf9871173f434f94bc79b73f4afde436c421983d
NVP-05 <147390576+NVP-05@users.noreply.github.com>
2024-01-15 07:40:21 +0700

    Add files via upload

---
+++ session23/index.js
@@ -0,0 +1,29 @@
+// for(let i = 0; i < 10; i++){    
+//     console.log(i);
+// }
+
+//tính tổng các số từ 1 đến 10
+
+// let sum = 0;
+// for(let i = 1; i < 11; i++){
+//     sum += i;
+// }
+// console.log("Tổng từ 1-->10 =" + " " + sum);
+
+// for(let i = 0; i < 5; i++){
+//     for(let j = 0; j < 5; j++){
+//         console.log(`Khi i = ${i} thì j = ${j}`);
+//     }
+// }
+
+// let i =0;
+// while (i <= 10) {
+//     console.log(i);
+//     i++;
+// }
+
+let n = 0;
+do {
+    console.log(n);
+    n++;
+} while(n <= 10);
